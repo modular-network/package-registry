e60cf0d7f11fba702c0a6cb8dc28ada1dfbb5850
Paulo Pires <taralloqueiroiz@gmail.com>
2017-11-28 11:46:13 -0200

    feat(publish/getByVersion): adds draft of two functions

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -1,21 +1,45 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.18;
+
+import './LinkedList.sol';
 
 contract Registry {
-  
-  struct Package {
-    string name;
-    string version;
-    string uri;
-  }
+    using LinkedListLib for LinkedListLib.LinkedList;
 
-  event NewPackagePublished(string name, string version, string uri);
-
-  function Registry {}
-
-  function getByVersion(string _version) {}
-  
-  function publish(string _name, string _version, string _uri) {
+    LinkedListLib.LinkedList packageList;
     
-    NewPackagePublished(_name, _version, _uri)
-  }
+    uint256 constant NULL = 0;
+    uint256 constant HEAD = 0;
+    bool constant PREV = false;
+    bool constant NEXT = true;
+
+    struct Package {
+        string name;
+        string version;
+        string uri;
+    }
+
+    uint256 currentIndex;
+
+    mapping(uint256 => Package) package;
+
+    event NewPackagePublished(string name, string version, string uri);
+
+    function getByVersion(string _name, string _version) public constant returns (string packageName, string packageVersion, string packageUri) {
+        for(uint256 i = 0; i <= currentIndex; i++) {
+            if(keccak256(package[i].name) == keccak256(_name) && keccak256(package[i].version) == keccak256(_version)){
+                packageName = package[i].name;
+                packageVersion = package[i].version;
+                packageUri = package[i].uri;
+            }
+        }
+        return (packageName, packageVersion, packageUri);
+    }
+    function publish(string _name, string _version, string _uri) public returns (uint256 packageIndex) {
+        packageIndex = currentIndex++;
+        package[packageIndex] = Package(_name, _version, _uri);
+        packageList.insert(packageIndex, HEAD, NEXT);
+        
+        NewPackagePublished(_name, _version, _uri);
+    }
+    
 }
